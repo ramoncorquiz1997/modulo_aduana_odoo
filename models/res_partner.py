6563c90b29fdec28300b6901b6eed7156cf769b2
RCO-E-SOL <110415007+RCO-E-SOL@users.noreply.github.com>
2026-02-12 12:18:42 -0800

    Agrego corregido lo del archvio del csf

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
 
-
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
     x_curp = fields.Char(string="CURP")
     x_identificacion_fiscal = fields.Char(string="Identificación fiscal (extranjero)")
+
+    # Documentos
+    x_csf_filename = fields.Char(string="Nombre de archivo CSF")
+    x_csf_file = fields.Binary(
+        string="CSF (PDF)", 
+        filename="x_csf_filename"  # Esto vincula el binario con el nombre
+    )
